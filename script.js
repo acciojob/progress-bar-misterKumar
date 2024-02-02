2c283b5f15d3e019ac47a31ec3a873529ba4bebc
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-02 08:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const circles = document.querySelectorAll(".circle");
+    const prevBtn = document.getElementById("prev");
+    const nextBtn = document.getElementById("next");
+    const lines = document.querySelectorAll(".line");
+
+    let currentIndex = 0;
+
+    updateButtonState();
+
+    prevBtn.addEventListener("click", function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateButtonState();
+        }
+    });
+
+    nextBtn.addEventListener("click", function () {
+        if (currentIndex < circles.length - 1) {
+            circles[currentIndex].classList.add("previous");
+            currentIndex++;
+            updateButtonState();
+        }
+    });
+
+    function updateButtonState() {
+        circles.forEach((circle, index) => {
+            if (index === currentIndex) {
+                circle.classList.add("active");
+            } else {
+                circle.classList.remove("active");
+            }
+
+			if(index < currentIndex)
+			{
+				circle.classList.add("previous");
+			}
+			else
+			{
+				circle.classList.remove("previous");
+			}
+
+			
+        });
+
+        lines.forEach((line, index) => {
+            if (index < currentIndex) {
+                line.classList.add("active");
+            } else {
+                line.classList.remove("active");
+            }
+        });
+
+        prevBtn.disabled = currentIndex === 0;
+        nextBtn.disabled = currentIndex === circles.length - 1;
+    }
+});
